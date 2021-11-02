e7fb2747057632f308645d84a2310fd5f739e02b
Abdulrahman Talafha <92855057+AbdulrahmanTalafha@users.noreply.github.com>
2021-11-02 16:34:41 +0200

    Summary

--- models/training.py
+++ models/training.py
@@ -1,5 +1,11 @@
 from odoo import models, fields, api,exceptions
 class training(models.Model):
     _name = 'training'
-    name = fields.Char()
+    name = fields.Char(string="Name", required=True)
+    age = fields.Integer(string="Age")
+    gender = fields.Selection([
+        ("male", "female")
+        ('female', 'male')
+        ('other' ,'Other')
+    ]). string ="Gender", default="male"
 

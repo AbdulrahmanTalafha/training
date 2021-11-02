83cdc0f05a4d7a375150be6e4906bd0745253132
Abdulrahman Talafha <92855057+AbdulrahmanTalafha@users.noreply.github.com>
2021-11-02 22:44:40 +0200

    Summary

--- models/training.py
+++ models/training.py
@@ -1,6 +1,7 @@
 from odoo import models, fields, api,exceptions
 class training(models.Model):
     _name = 'training'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     name = fields.Char(string="Name", required=True)
     age = fields.Integer(string="Age")
     gender = fields.Selection([
@@ -8,4 +9,5 @@ class training(models.Model):
         ('female', 'Female'),
         ('other' ,'Other'),
     ], string ="Gender", default="male")
+    image = field.Binary(string="image")
 

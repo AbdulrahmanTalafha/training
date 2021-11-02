ad9daec19a5ab52b584660e495204b10a39356ec
Abdulrahman Talafha <92855057+AbdulrahmanTalafha@users.noreply.github.com>
2021-11-02 15:55:54 +0200

    Update __manifest__.py

--- __manifest__.py
+++ __manifest__.py
@@ -10,8 +10,8 @@
         Long description of module's purpose
     """,
 
-    'author': "My Company",
-    'website': "http://www.yourcompany.com",
+    'author': "Abdulrahman Talafha",
+    'website': "http://www.techsfactory.com",
 
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml

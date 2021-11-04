678776b13df99d3c69979c4f19d54cc0f1cef4d1
Abdulrahman Talafha <92855057+AbdulrahmanTalafha@users.noreply.github.com>
2021-11-04 18:46:08 +0200

    Summary

--- school/models/school.py
+++ school/models/school.py
@@ -1,14 +1,15 @@
-from odoo import models, fields, api,exceptions
-class training(models.Model):
+from typing_extensions import Required
+from odoo import models,fields,api,exception
+
+class school(models.Model):
     _name = 'school'
     _inherit = ['mail.thread', 'mail.activity.mixin']
-    
-    name = fields.Char(string="Name", required=True)
-    age = fields.Integer(string="Age")
-    gender = fields.Selection([
-        ("male", "Male"),
-        ('female', 'Female'),
-        ('other' ,'Other'),
-    ], string ="Gender", default="male")
-    image = fields.Binary(string="image")
 
+    name = fields.Char(string='Name', Required=True)
+    age = fields.Integer(string='Age')
+    gender = fields.Selection([
+        ('male', 'Male')
+        ('female', 'Female')
+        ('other', 'Other')
+    ], string='Gender', default="Male")
+    image = fields.Binary(string='Image')

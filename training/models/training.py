64451751c760c1ef8a41794db4b24931159f4177
Abdulrahman Talafha <92855057+AbdulrahmanTalafha@users.noreply.github.com>
2021-11-06 18:33:25 +0200

    Update training.py

--- training/models/training.py
+++ training/models/training.py
@@ -5,7 +5,7 @@ class training(models.Model):
     _description = 'Training'
     
     name = fields.Char(string = "Name", required = True)
-    age = fields.Char(string="Age")
+    age = fields.Integer(string="Age")
     gender = fields.Selection([
         ("male", "Male"),
         ('female', 'Female'),

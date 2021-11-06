1514dffe63200259e42a9fca3a6144b872e30d79
Abdulrahman Talafha <92855057+AbdulrahmanTalafha@users.noreply.github.com>
2021-11-06 16:33:08 +0200

    Summary

--- school/models/school.py
+++ school/models/school.py
@@ -4,13 +4,12 @@ from odoo import models,fields,api,exception
 class school(models.Model):
     _name = 'school'
     _inherit = ['mail.thread', 'mail.activity.mixin']
-    _description = 'School'
 
-    name = fields.Char(string='Name')
-    age = fields.Integer(string='Age')
-    gender = fields.Selection([
+    namee = fields.Char(string='Name')
+    agee = fields.Integer(string='Age')
+    genderr = fields.Selection([
         ('male', 'Male')
         ('female', 'Female')
         ('other', 'Other')
     ], string='Gender', default="Male")
-    image = fields.Binary(string='Image')
+    imagee = fields.Binary(string='Image')

cb8e6ea74d82b9531cd05a90f47f7c332cce5b42
Abdulrahman Talafha <92855057+AbdulrahmanTalafha@users.noreply.github.com>
2021-11-06 19:52:41 +0200

    Summary

--- training/models/training.py
+++ training/models/training.py
@@ -1,4 +1,6 @@
 from odoo import models, fields, api,exceptions
+from odoo.addons.website.tools import get_video_embed_code
+
 class training(models.Model):
     _name = 'training'
     _inherit = ['mail.thread', 'mail.activity.mixin']
@@ -13,4 +15,10 @@ class training(models.Model):
     ], string = "Gender", default = "male")
     image = fields.Binary(string = "image")
     active = fields.Boolean(string='Active',default=True)
+    video_url = fields.Char('Video URL', help="URL of a video")
+    embed_code = fields.Char(compute="_compute_embed_code")
 
+    @api.depends('video_url')
+    def _compute_embed_code(self):
+        for rec in self:
+            rec.embed_code = get_video_embed_code(rec.video_url)

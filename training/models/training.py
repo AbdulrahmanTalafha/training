4003335d39881d66c35fa4b7de063449ade5b389
Abdulrahman Talafha <92855057+AbdulrahmanTalafha@users.noreply.github.com>
2021-11-14 21:14:34 +0200

    Summary

--- training/models/training.py
+++ training/models/training.py
@@ -20,6 +20,8 @@ class training(models.Model):
     video_url = fields.Char('Video URL', help="URL of a video for showcasing your student.")
     embed_code = fields.Char(compute="_compute_embed_code")
 
+    pageadd = fields.One2many(string="Add lines")
+
     @api.depends('video_url')
     def _compute_embed_code(self):
         for rec in self:
